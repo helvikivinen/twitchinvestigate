75766c0d43c88e331b0c952fe4bbb2266f4b80e1
Helvi Kivinen <helvikivinen@gmail.com>
2023-08-14 01:02:09 +0200

    renames twitch_name column in table definitions file

--- viewer.py
+++ viewer.py
@@ -3,9 +3,10 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class Viewer(Base):
     __tablename__ = "viewers"
-    id=Column(Integer, primary_key=True)
-    twitch_id=Column(Integer)
-    twitch_display_name=Column(String)
-    channel_points=Column(Integer)
+    id = Column(Integer, primary_key=True)
+    twitch_id = Column(Integer)
+    twitch_name = Column(String)
+    channel_points = Column(Integer)
