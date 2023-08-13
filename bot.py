e1fa469c5544e10d383ead703040ee9d71f7ac72
Helvi Kivinen <helvikivinen@gmail.com>
2023-08-13 21:10:58 +0200

    adds event_message function to prevent bot from replying to itself

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 
 load_dotenv()
 
+
 class Bot(commands.Bot):
     def __init__(self):
         apikey = os.getenv("API_KEY")
@@ -11,13 +12,26 @@ class Bot(commands.Bot):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token=apikey, prefix='?', initial_channels=['...'])
-    
+        super().__init__(token=apikey, prefix="?", initial_channels=["..."])
+
     async def event_ready(self):
         # Notify us when everything is ready!
         # We are logged in and ready to chat and use commands...
-        print(f'Logged in as | {self.nick}')
-        print(f'User ID is | {self.user_id}')
+        print(f"Logged in as | {self.nick}")
+        print(f"User ID is | {self.user_id}")
+
+    async def event_message(self, message):
+        # Messages with echo set to True are messages sent by the bot...
+        # For now we just want to ignore them...
+        if message.echo:
+            return
+
+        # Print the contents of our message to console...
+        print(message.content)
+
+        # Since we have commands and are overriding the default `event_message`
+        # We must let the bot know we want to handle and invoke our commands...
+        await self.handle_commands(message)
 
     @commands.command()
     async def hello(self, ctx: commands.Context):
@@ -27,7 +41,8 @@ class Bot(commands.Bot):
 
         # Send a hello back!
         # Sending a reply back to the channel is easy... Below is an example.
-        await ctx.send(f'Hello {ctx.author.name}!')
+        await ctx.send(f"Hello {ctx.author.name}!")
+
 
 bot = Bot()
 bot.run()
