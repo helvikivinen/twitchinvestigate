808b34a6e529839a750c13aebfbc9a2d7e239f54
Helvi Kivinen <helvikivinen@gmail.com>
2023-08-13 22:45:38 +0200

    adds a list of valid commands and checks against them whether or not to intact with the message

--- bot.py
+++ bot.py
@@ -13,16 +13,18 @@ db_file = "sqlite:///database.sqlite3"
 
 engine = create_engine(db_file, echo=True)
 Session = sessionmaker(bind=engine)
+command_prefix = "?"
+commandlist = [command_prefix + "hello", command_prefix + "repeat"]
+
 
 class Bot(commands.Bot):
     def __init__(self):
         apikey = os.getenv("API_KEY")
-        channel_name = os.getenv("CHANNEL_NAME")        
+        channel_name = os.getenv("CHANNEL_NAME")
 
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token=apikey, prefix="?", initial_channels=[channel_name])
+        super().__init__(
+            token=apikey, prefix=command_prefix, initial_channels=[channel_name]
+        )
 
     async def event_ready(self):
         # Notify us when everything is ready!
@@ -31,32 +33,48 @@ class Bot(commands.Bot):
         print(f"User ID is | {self.user_id}")
 
     async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
-        if message.echo:
+        # first_word here is just message content so if a valid command, it WILL contain the command_prefix
+        first_word = message.content.split(" ", 1)[0]
+
+        # if message.echo is true, it's a message by the bot
+        # also, ignore messages that don't match a command in our commandlist
+        # or first_word not in commandlist
+
+        # print(f"first_word: {first_word}")
+        # print(f"message_content: {message.content}")
+
+        if message.echo or first_word not in commandlist:
             return
 
-        # Print the contents of our message to console...
+        print("event_message: ", end="")
         print(message.content)
 
-        # Since we have commands and are overriding the default `event_message`
-        # We must let the bot know we want to handle and invoke our commands...
         await self.handle_commands(message)
 
     @commands.command()
     async def hello(self, ctx: commands.Context):
         session = Session()
-        print("Session Created")        
-        viewerExists = session.query(session.query(Viewer).filter(Viewer.twitch_id == ctx.author.id).exists()).scalar()
+        print("Session Created")
+        viewerExists = session.query(
+            session.query(Viewer).filter(Viewer.twitch_id == ctx.author.id).exists()
+        ).scalar()
 
         if not viewerExists:
-            viewer = Viewer(twitch_id = ctx.author.id, twitch_display_name = ctx.author.display_name, channel_points = 0)
+            viewer = Viewer(
+                twitch_id=ctx.author.id,
+                twitch_display_name=ctx.author.display_name,
+                channel_points=0,
+            )
             session.add(viewer)
             session.commit()
 
         await ctx.send(f"Hello {ctx.author.name}!")
 
+    @commands.command()
+    async def repeat(self, ctx: commands.Context):
+        # ctx.command.name here does NOT contain the command_prefix
+        await ctx.send(f"You used the command: {ctx.command.name}")
+
 
 bot = Bot()
 bot.run()
-# bot.run() is blocking and will stop execution of any below code here until stopped or closed.
