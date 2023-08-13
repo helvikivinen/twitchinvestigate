c5ba492a07362e450ac5ac5ba30efefd14027148
Skelechan <skelechanvt@gmail.com>
2023-08-13 19:46:29 +0100

    Init commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+from twitchio.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Bot(commands.Bot):
+    def __init__(self):
+        apikey = os.getenv("API_KEY")
+
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        # prefix can be a callable, which returns a list of strings or a string...
+        # initial_channels can also be a callable which returns a list of strings...
+        super().__init__(token=apikey, prefix='?', initial_channels=['...'])
+    
+    async def event_ready(self):
+        # Notify us when everything is ready!
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+        print(f'User ID is | {self.user_id}')
+
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        # Here we have a command hello, we can invoke our command with our prefix and command name
+        # e.g ?hello
+        # We can also give our commands aliases (different names) to invoke with.
+
+        # Send a hello back!
+        # Sending a reply back to the channel is easy... Below is an example.
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+bot = Bot()
+bot.run()
+# bot.run() is blocking and will stop execution of any below code here until stopped or closed.
